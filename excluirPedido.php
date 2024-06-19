af1d4a51c5f066e6edb71de91e7d75cff5b08b39
Yasmin Martins Bertoncini <144287930+YasminBertoncini@users.noreply.github.com>
2024-06-19 09:55:21 -0300

    Update excluirPedido.php

--- excluirPedido.php
+++ excluirPedido.php
@@ -1,10 +1,10 @@
 <?php
-require 'conectaBanco.php'; // Arquivo de conexão com o banco de dados
+require 'conectaBanco.php'; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['pedido_id'])) {
     $pedido_id = $_POST['pedido_id'];
 
-    // Query para excluir o pedido
+    
     $sql = "DELETE FROM pedidos WHERE id = $pedido_id";
 
     if ($banco->query($sql) === TRUE) {
